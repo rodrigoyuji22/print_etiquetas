f3467fb8e2dc84281197df68711843befe255c4a
Rodrigo <rodrigoyujik@gmail.com>
2025-10-02 09:17:21 -0300

    rename

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from db import run_query
 from label_service import render_label
 from pathlib import Path
 
-query_path = Path("queries/label.sql")
-zpl_path = Path("templates/label.zpl")
+query_path = Path("queries/expedicao.sql")
+zpl_path = Path("templates/expedicao.zpl")
 def main():
     df = run_query(63311, "7")
     z = render_label(df, zpl_path)
