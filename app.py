730ced6b092e5fb1113b5f3ddf17f0cbd4d4e6c8
Rodrigo <rodrigoyujik@gmail.com>
2025-10-06 11:49:18 -0300

    .

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 
 printerTransporte, printerExpedicao, printerEstoque = os.getenv("PRINTER_TRA"), os.getenv("PRINTER_EXPEDICAO"), os.getenv("PRINTER_GALPAO2")
-template_transporte, templeta_expedicao, template_estoque = Path("templates/transito.zpl"), Path("templates/expedicao.zpl"), Path("templates/estoque.zpl")
+template_transporte, template_expedicao, template_estoque = Path("templates/transito.zpl"), Path("templates/expedicao.zpl"), Path("templates/estoque.zpl")
 host, port = os.getenv("HOST_API"), int(os.getenv("PORT_API", 1234))
 
 
@@ -50,6 +50,59 @@ def print_transporte():
             'message': str(e)
         }), 500
 
+@app.route('/print/estoque', methods=['POST'])
+def print_estoque():
+    try:
+        data = request.get_json()
+        itemCode = data.get('itemCode')
+        lote = data.get('lote')
+        qtd = data.get('qtd')
+        peso = data.get('peso')
+
+        if not itemCode:
+            return jsonify({'error': 'Campo Código do item obrigatório'}), 400
+        df = run_query_est(itemCode)
+        zpl = render_labels(df, template_estoque, qtd, peso)
+        print_(zpl, printerExpedicao)
+        return jsonify({
+            'status': 'success', 
+            'message': 'Etiqueta inserida na fila de impressao'
+        }), 201
+
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': str(e)
+        }), 500
+    
+@app.route('/print/expedicao', methods=('POST')) # type: ignore
+def print_expedicao():
+    try:
+        data = request.get_json()
+        pv = data.get('pv')
+        itemCode = data.get('itemCode')
+        qtd = data.get('qtd')
+        peso = data.get('peso')
+        lote = data.get('lote')
+
+        df = run_query_exp(pv, itemCode)
+        if df.empty: # pyright: ignore[reportOptionalMemberAccess]
+            return jsonify({'error': 'insira um pv valido'}), 404
+        zpl = render_labels(df, template_expedicao, qtd, peso)
+        print_(zpl, printerExpedicao)
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': str(e)
+        }), 500
+    
+        
+
+
+
+
+
+
         
 if __name__ == '__main__':
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port, debug=True, threaded=True)
