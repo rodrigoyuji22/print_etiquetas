1f5bb4c2f440056e135822679395f0449b98c9b9
rodrigoyuji22 <rodrigoyujik@gmail.com>
2025-10-09 16:04:19 -0300

    landing page

--- frontend/interface.py
+++ frontend/interface.py
@@ -1,41 +1,76 @@
 import streamlit as st
 
-API_URL = "http://192.168.1.90:7000"
-
-st.set_page_config(
-    page_title="Etiquetas",
-    page_icon="🏭",
-    layout="centered"
-)
+st.set_page_config(page_title="Etiquetas", page_icon="🏭", layout="centered")
 
 st.markdown("""
 <style>
+/* Box central */
+.block-container {
+  background: #282B2E;
+  border: 1px solid #2b2d30;
+  border-radius: 16px;
+  box-shadow: 0 12px 40px rgba(0,0,0,.45);
+  padding: 48px 56px;
+  max-width: 720px;
+  margin-top: 60px;
+  text-align: center;
+}
+
+/* ======== Titulo e subtitulo  ======== */
+h1 {
+  text-align: center !important;
+  font-size: 2.4rem !important;
+  font-weight: 1200 !important;
+  color: #ffffff !important;
+  margin-bottom: 10px;
+}
+
+p {
+  text-align: center !important;
+  font-size: 1.2rem !important;
+  color: #d0d0d0 !important;
+}
+
+/* ======== BOTÕES ======== */
 div.stButton > button {
-    background-color: #2c2f33;
-    color: white;
-    border-radius: 8px;
-    height: 3em;
-    font-weight: 600;
-    border: 1px solid #2b2d30;
-    transition: 0.2s;
+  background-color: #2c2f33;
+  color: #fff;
+  border-radius: 8px;
+  height: 4em;
+  font-weight: 600;
+  border: 1px solid #2b2d30;
+  transition: 0.2s;
+}
+div.stButton > button > div > p {
+  font-size: 26px !important;
+  font-weight: bold;
 }
 div.stButton > button:hover {
-    background-color: #5b96ff;
-    transform: scale(1.02);
+  background-color: #4c8bf5;
+  transform: scale(1.02);
 }
 </style>
-""", unsafe_allow_html = True)
+""", unsafe_allow_html=True)
 
-st.title("Impressão de etiquetas!")
-st.markdown('### Selecione abaixo o tipo de etiqueta que deseja gerar:')
-st.write("---")
+st.title("Impressão de Etiquetas")
+st.write("Escolha o tipo de etiqueta que deseja imprimir:")
 
-if st.button("🚚 Transporte", use_container_width=True):  # use_container_width eh pra ocupar o botao inteiro
-        st.switch_page("pages/transporte.py")
-st.markdown("")
-if st.button("📦 Expedição", use_container_width=True):  # use_container_width eh pra ocupar o botao inteiro
-        st.switch_page("pages/expedicao.py")
-st.markdown("")
+if st.button("🚚 Transporte", use_container_width=True):
+    st.switch_page("pages/transporte.py")
 
-if st.button("🏷️ Estoque", use_container_width=True):  # use_container_width eh pra ocupar o botao inteiro
+st.write("")
+if st.button("📦 Expedição", use_container_width=True):
+    st.switch_page("pages/expedicao.py")
+
+st.write("")
+if st.button("🏷️ Estoque", use_container_width=True):
     st.switch_page("pages/estoque.py")
+
+st.write("")
+if st.button("DEXCO", use_container_width=True):
+    st.switch_page("pages/dexco.py")
+
+st.write("")
+st.write("")
+st.caption("Versão de testes ainda")
+st.write("")
