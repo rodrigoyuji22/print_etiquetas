bf08a5917481ca6c714057c315f849dabe7ccfe2
rodrigoyuji22 <rodrigoyujik@gmail.com>
2025-10-09 23:26:58 -0300

    feat: styles + transporte page

--- frontend/interface.py
+++ frontend/interface.py
@@ -2,56 +2,11 @@ import streamlit as st
 
 st.set_page_config(page_title="Etiquetas", page_icon="🏭", layout="centered")
 
-st.markdown("""
-<style>
-/* Box central */
-.block-container {
-  background: #282B2E;
-  border: 1px solid #2b2d30;
-  border-radius: 16px;
-  box-shadow: 0 12px 40px rgba(0,0,0,.45);
-  padding: 48px 56px;
-  max-width: 720px;
-  margin-top: 60px;
-  text-align: center;
-}
-
-/* ======== Titulo e subtitulo  ======== */
-h1 {
-  text-align: center !important;
-  font-size: 2.4rem !important;
-  font-weight: 1200 !important;
-  color: #ffffff !important;
-  margin-bottom: 10px;
-}
-
-p {
-  text-align: center !important;
-  font-size: 1.2rem !important;
-  color: #d0d0d0 !important;
-}
-
-/* ======== BOTÕES ======== */
-div.stButton > button {
-  background-color: #2c2f33;
-  color: #fff;
-  border-radius: 8px;
-  height: 4em;
-  font-weight: 600;
-  border: 1px solid #2b2d30;
-  transition: 0.2s;
-}
-div.stButton > button > div > p {
-  font-size: 26px !important;
-  font-weight: bold;
-}
-div.stButton > button:hover {
-  background-color: #4c8bf5;
-  transform: scale(1.02);
-}
-</style>
-""", unsafe_allow_html=True)
-
+def load_css():
+  with open("style/style.css", "r") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    
+load_css()  
 st.title("Impressão de Etiquetas")
 st.write("Escolha o tipo de etiqueta que deseja imprimir:")
 
---
+++ frontend/pages/transporte.py
@@ -0,0 +1,29 @@
+import requests
+import streamlit as st
+from interface import load_css
+
+api_url = st.secrets["api"]["url"]
+
+load_css()
+
+with st.form("forms_transporte"):
+    st.title("Etiqueta de Transporte")
+    nf = st.text_input("Número da NF:")
+    vol = st.text_input("Volume:")
+    submitted = st.form_submit_button("Imprimir Etiqueta")
+
+    if submitted:
+        if not nf:
+            st.warning("Insira NF válida")
+        else:
+            data = {'nf': nf, 'vol': vol}
+            try:
+                response = requests.post(f"{api_url}/print/transporte", json=data)
+                if response.status_code == 200:
+                    st.success("✅ Etiquetas inseridas na fila!")
+            except Exception as e:
+                st.error("NF inválida")
+
+
+
+
